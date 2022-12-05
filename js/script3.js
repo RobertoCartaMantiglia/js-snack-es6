8dd48b4cca3dfbc18f8bd6d16196c213988299c7
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-05 12:35:24 +0100

    zucchine

---
+++ js/script3.js
@@ -0,0 +1,62 @@
+/**
+ *
+ * Crea 10 oggetti che rappresentano una zucchina, 
+ * indicandone per ognuno varietà, peso e lunghezza.
+ *
+ * Calcola quanto pesano tutte le zucchine.
+ *
+ */
+
+const zucchine = [
+    
+    {
+        type : "cicciotta",
+        peso : 2,
+        lunghezza : 10,
+    },
+    {
+        type : "secca",
+        peso : 0.5,
+        lunghezza : 20,
+    },
+    {
+        type : "cicciotta",
+        peso : 0.8,
+        lunghezza : 12,
+    },
+    {
+        type : "secca",
+        peso : 1,
+        lunghezza : 16,
+    },
+    {
+        type : "normale",
+        peso : 0.8,
+        lunghezza : 15,
+    },
+    {
+        type : "normale",
+        peso : 0.6,
+        lunghezza : 14,
+    },
+    {
+        type : "cicciotta",
+        peso : 1,
+        lunghezza : 17,
+    },
+    {
+        type : "secca",
+        peso : 0.4,
+        lunghezza : 8,
+    },
+    {
+        type : "nomale",
+        peso : 0.5,
+        lunghezza : 12,
+    },
+    {
+        type : "cicciotta",
+        peso : 1.2,
+        lunghezza : 19,
+    },
+];
