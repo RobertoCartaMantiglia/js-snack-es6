cea8742565bfd4aa859d092b4f153db76c02231f
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-05 12:00:04 +0100

    array automobili

--- js/script2.js
+++ js/script2.js
@@ -6,3 +6,57 @@
    Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
    Infine stampa separatamente i 3 array.
  */
+
+
+   const automobili = [
+        {
+            marca : "alfa romeo",
+            modello: "159",
+            alimentazione: "diesel",
+        },
+        {
+            marca : "lamborghini",
+            modello: "hurus",
+            alimentazione: "benzina",
+        },
+        {
+            marca : "renault",
+            modello: "twingo",
+            alimentazione: "gpl",
+        },
+        {
+            marca : "peugeot",
+            modello: "107",
+            alimentazione: "benzina",
+        },
+        {
+            marca : "dodge",
+            modello: "ram",
+            alimentazione: "diesel",
+        },
+        {
+            marca : "toyota",
+            modello: "yaris",
+            alimentazione: "hybrid",
+        },
+        {
+            marca : "lancia",
+            modello: "ypsilon",
+            alimentazione: "hybrid",
+        },
+        {
+            marca : "nissan",
+            modello: "xtrail",
+            alimentazione: "diesel",
+        },
+        {
+            marca : "volvo",
+            modello: "c30",
+            alimentazione: "diesel",
+        },
+        {
+            marca : "audi",
+            modello: "q5",
+            alimentazione: "diesel",
+        },
+   ]
