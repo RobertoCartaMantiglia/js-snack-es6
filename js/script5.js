387780eb6cba9c114f2d061f16b1a7826a00aaaf
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-06 11:57:22 +0100

    array people complete

---
+++ js/script5.js
@@ -0,0 +1,61 @@
+/***
+ * 
+Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un'età, creiamone almeno una decina.
+
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+
+ */
+
+const persone = [
+    {
+        nome: "Tonino",
+        cognome: "Carramone",
+        age: 22,
+    },
+    {
+        nome: "Marco",
+        cognome: "Pillittu",
+        age: 28,
+    },
+    {
+        nome: "Gaetano",
+        cognome: "Migliore",
+        age: 72,
+    },
+    {
+        nome: "Mirko",
+        cognome: "Coacella",
+        age: 38,
+    },
+    {
+        nome: "Giangiusto",
+        cognome: "Calabresi",
+        age: 80,
+    },
+    {
+        nome: "Gian Gianni",
+        cognome: "Mariottide",
+        age: 55,
+    },
+    {
+        nome: "Mario",
+        cognome: "Mariani",
+        age: 72,
+    },
+    {
+        nome: "Cristoforo",
+        cognome: "Colombo",
+        age: 90,
+    },
+    {
+        nome: "Marcello",
+        cognome: "Marcellini",
+        age: 43,
+    },
+    {
+        nome: "Romolo",
+        cognome: "Mastropasqualini",
+        age: 85,
+    },
+];
