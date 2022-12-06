94a586b8b5294dfffe8b99f2ad6033866fa2235c
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-06 11:30:05 +0100

    create array animals

---
+++ js/script4.js
@@ -0,0 +1,67 @@
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+// Es:
+
+// [
+//   { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+//   { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+//   { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+// ]
+
+// Crea un nuovo array con la lista dei mammiferi.
+// Bonus 1 : trasforma ogni valore di ogni animale in tutto maiuscolo tranne la prima lettera, tipo // leone => lEONE, canidi => cANIDI
+
+
+const animals = [
+    {
+        nome: "cane",
+        famiglia: "canidi",
+        classe: "mammiferi",
+    },
+    {
+        nome: "leone",
+        famiglia: "felini",
+        classe: "mammiferi",
+    },
+    {
+        nome: "piccione",
+        famiglia: "volatili",
+        classe: "uccelli",
+    },
+    {
+        nome: "mucca",
+        famiglia: "ruminanti",
+        classe: "mammiferi",
+    },
+    {
+        nome: "gatto",
+        famiglia: "felini",
+        classe: "mammiferi",
+    },
+    {
+        nome: "serpente",
+        famiglia: "rettili",
+        classe: "ovipari",
+    },
+    {
+        nome: "aquila",
+        famiglia: "volatili",
+        classe: "uccelli",
+    },
+    {
+        nome: "giaguaro",
+        famiglia: "felini",
+        classe: "mammiferi",
+    },
+    {
+        nome: "balena",
+        famiglia: "cetaceo",
+        classe: "mammiferi",
+    },
+    {
+        nome: "quaglia",
+        famiglia: "volatili",
+        classe: "uccelli",
+    },
+];
+console.log(animals);
